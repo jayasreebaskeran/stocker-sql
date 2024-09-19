6ef123a2233ba8261e1e0e800aaf5ad9db918ca8
Vinay Kumar Kosgi <vinaykumar.kosgi@outlook.com>
2024-09-19 16:39:24 +0530

    updated testing.py

--- testing.py
+++ testing.py
@@ -1,13 +1,13 @@
-   from flask import Flask
-   from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-   app = Flask(__name__)
-   app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Ut2DBsHFM1JZEdEr8mvn@database-1.cp8osk6oad8n.ap-south-1.rds.amazonaws.com/stocker'
-   db = SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Ut2DBsHFM1JZEdEr8mvn@database-1.cp8osk6oad8n.ap-south-1.rds.amazonaws.com/stocker'
+db = SQLAlchemy(app)
 
-   @app.route('/')
-   def index():
-       return "Database connection successful!"
+@app.route('/')
+def index():
+    return "Database connection successful!"
 
-   if __name__ == '__main__':
-       app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
